df069a72a7246c199ee0e4896384ba0de3a46766
michee <micheebakanda@gmail.com>
2022-02-02 11:04:58 +0200

    Footer complete

--- components/Map.js
+++ components/Map.js
@@ -1,16 +1,17 @@
-
 const Map = () => {
-    return (
-        <div>
-                <img src="https://www.w3schools.com/w3images/map.jpg" style={{
-                    width: "100%",
-                    height: "759px",
-                    objectFit: "cover",
-                    filter: "grayscale(50%)"
-                  }} />
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <img
+        src="https://www.w3schools.com/w3images/map.jpg"
+        style={{
+          width: "100%",
+          height: "759px",
+          objectFit: "cover",
+          filter: "grayscale(50%)",
+        }}
+      />
+    </div>
+  );
+};
 
 export default Map;
