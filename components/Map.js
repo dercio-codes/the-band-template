268f4b6042dc666576ff48ec82a9409e8323e11a
Dercio Maduna <76833905+derciomaduna-codes@users.noreply.github.com>
2022-02-08 11:40:28 +0200

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -3,6 +3,7 @@ const Map = () => {
     <div>
       <img
         src="https://www.w3schools.com/w3images/map.jpg"
+    alt="
         style={{
           width: "100%",
           height: "759px",
