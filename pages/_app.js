9eba7564c3534667d4339f9fdc14016340f1b5eb
Dercio Maduna <76833905+derciomaduna-codes@users.noreply.github.com>
2022-02-08 11:27:10 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { ThemeProvider } from "@emotion/styled";
+import { ThemeProvider } from "@mui/material/styles'";
 
 function MyApp({ Component, pageProps }) {
   return (
